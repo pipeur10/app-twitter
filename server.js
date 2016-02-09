bfae9426e200a480971120d5bed5e9afd0375f86
Philip <philippenathey@gmail.com>
2016-02-09 11:22:14 +0000

    commit

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var express = require('express'),
 app.use(express.static('www'));
 
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
-});
+// app.all('*', function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//     next();
+// });
 
 // API Routes
 // app.get('/blah', routeHandler);
